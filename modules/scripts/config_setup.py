0338ccb8c7defd0b0817a1bc6c080ac86cfe09e6
Vanessa Mandel <vanessa.mandel@helmholtz-munich.de>
2024-07-03 11:30:22 +0200

    adapted config file and functions to be able to optionally read samples from csv file instead of writing them into the config file

--- modules/scripts/config_setup.py
+++ modules/scripts/config_setup.py
@@ -8,13 +8,20 @@
 # @email: vangalamaheshh@gmail.com
 # @date: July, 1st, 2016
 #------------------------------------
+""" Description:
+    Function to ensure the config values used by Snakemake are set up correctly with necessary paths and formatted values.
+    - Load reference paths from YAML file into configuration
+    - Force Snakemake to recognize changes
+    - Add necessary executable paths to configuration
+"""
 
 import yaml
 import os, sys, subprocess
 
 def updateConfig(config):
     loadRef(config)
-    config["config_file"] = "config.yaml" # trick to force rules on config change	
+    config["config_file"] = "config.yaml" # trick to force rules on config change --> trigger Snakemake to recognize that the configuration has changed and thus re-evaluate relevant rules	
+    # convert values of the specific keys to strings
     for k in ["RPKM_threshold","min_num_samples_expressing_at_threshold", 
                 "numgenes_plots","num_kmeans_clust","filter_mirna"]:
         config[k] = str(config[k])
@@ -26,19 +33,25 @@ def updateConfig(config):
 # CHIPS-like refs file
 #-----------------------------------------------------------------------------
 def loadRef(config):
-    """Adds the static reference paths found in config['ref']
-    NOTE: if the elm is already defined, then we DO NOT clobber the value
+    """Opens and reads the reference YAML file found in config['ref'] --> Adds the static reference paths found in this file
+    NOTE: if the element is already defined, then we DO NOT clobber the value
     """
     f = open(config['ref'])
-    ref_info = yaml.safe_load(f)
+    ref_info = yaml.safe_load(f) # Loads the YAML content
     f.close()
     #print(ref_info[config['assembly']])
-    for (k,v) in ref_info[config['assembly']].items():
+    for (k,v) in ref_info[config['assembly']].items(): # ref_info["hg19"] (e.g. for config["assembly"] = "hg19") --> all paths specified for hg19 assembly
         #NO CLOBBERING what is user-defined!
         if k not in config:
             config[k] = v
 
 def _addExecPaths(config):
+    '''
+    Function to add or update paths to various executables and environments in the configuration
+        Get root path of Conda installation, sets python2 sibrary path for `viper_py2` conda environment
+        checks and sets paths for various executables (python2, rseqc_path, picard_path, varscan_path, trust_path, optitype_path) if they are not already defined.)
+    @return The updated config file
+    '''
     conda_root = subprocess.check_output('conda info --root',shell=True).decode('utf-8').strip()
     conda_path = os.path.join(conda_root, 'pkgs')
     #NEED the following when invoking python2 (to set proper PYTHONPATH)
--- modules/scripts/metasheet_setup.py
+++ modules/scripts/metasheet_setup.py
@@ -8,23 +8,47 @@
 # @email: vangalamaheshh@gmail.com
 # @date: July, 1st, 2016
 #-------------------------------------
+# =============================================================================
+# Copyright: Copyright 2024, viper_adaption
+# License: GPL
+# Version: v0.1
+# Editor: Vanessa Mandel
+# Email: schmollv@mytum.de
+# Created: 29.05.2024 (DD.MM.YYYY)
+# Last Modified: 
+# =============================================================================
+""" Description:
+    Update metadata information in Snakemake's config dict
+    - remove invalid characters and convert to unix line endings inside the metasheet csv file
+    - Extract column names starting with `comp_`
+    - add file name(s) for each sample to the config dict
+        - edited by Vanessa Mandel: Give option to only specify a path to fastq files (config["fastq_path_prefix"]) in the config.yaml file
+            or specify the sample names and samples (see commented option in standard config.yaml file)
+        - if only path is specified the SampleName column from the metasheet is used to search for existing files (i.e. config["fastq_path_prefix"]{sample}/{sample}*.fastq(.gz)?)
+"""
 
 import pandas as pd
 from collections import defaultdict
+import warnings
+import glob
 
 def updateMeta(config):
     _sanity_checks(config)
-    metadata = pd.read_table(config['metasheet'], index_col=0, sep=',', comment='#')
-    config["comparisons"] = [c[5:] for c in metadata.columns if c.startswith("comp_")]
+    metadata = pd.read_table(config['metasheet'], index_col=0, sep=',', comment='#') # read metadata csv file
+    config["comparisons"] = [c[5:] for c in metadata.columns if c.startswith("comp_")] # extract column names starting with `comp_` and remove the five characters `comp_`
     config["comps"] = _get_comp_info(metadata)
-    config["metacols"] = [c for c in metadata.columns if c.lower()[:4] != 'comp']
-    config["file_info"] = { sampleName : config["samples"][sampleName] for sampleName in metadata.index }
+    config["metacols"] = [c for c in metadata.columns if c.lower()[:4] != 'comp'] # add names of the metadata columns to config
+    # check if fastq_path_prefix is specified in the config file under the samples key
+    if "fastq_path_prefix" in config["samples"]:
+        _update_sample_file_info(config,metadata)
+    else : # assume that sample names and their corresponding files are specified in config file
+        config["file_info"] = { sampleName : config["samples"][sampleName] for sampleName in metadata.index } # extract info with file paths specified in the config.yaml file for all samples specified in the metadata csv file
     config["ordered_sample_list"] = metadata.index
     return config
 
 
 def _sanity_checks(config):
-    #metasheet pre-parser: converts dos2unix, catches invalid chars
+    #metasheet pre-parser: converts dos2unix line endings, catches invalid chars
     _invalid_map = {'\r':'\n', '(':'.', ')':'.', ' ':'_', '/':'.', '$':''}
     _meta_f = open(config['metasheet'])
     _meta = _meta_f.read()
@@ -45,6 +69,9 @@ def _sanity_checks(config):
 
 
 def _get_comp_info(meta_info):
+    ''' create dictionary with comparison name (respective column name with out `comp_` prefix) and the sample names corresponding to control (1) and treatment (2) groups. 
+        comps_info looks something like this: {comp_name:{'control':['sample1','sample3',...], 'treat':['sample2',...]}, comp_name:...}
+    '''
     comps_info = defaultdict(dict)
     for comp in meta_info.columns:
         if comp[:5] == 'comp_':
@@ -52,3 +79,24 @@ def _get_comp_info(meta_info):
             comps_info[comp[5:]]['treat'] = meta_info[meta_info[comp] == 2].index
     return comps_info
 
+def _update_sample_file_info(config,metadata):
+    """ dynamically locate and use FASTQ files based on the metadata and specified directory prefix and add it to config
+    """
+    # extract the prefix
+    prefix = config["samples"]["fastq_path_prefix"]
+    # Initialize samples dictionary if not already present
+    if len(config["samples"]) == 1:
+        pass
+    else:
+        warnings.warn("Additional entries in the config file for samples other than `fastq_path_prefix`. Will extract the sample names from the metasheet and search for the respective files specified in the `fastq_path_prefix`.", UserWarning)
+    # iterate over samples, extract file names and save them in the config["samples"] dictionary
+    config["samples"] = {} # empty all entries in samples
+    for sample in metadata.index:
+        search_str = "/".join((prefix+sample,f"{sample}*.fastq.*"))
+        fastq_files = glob.glob(search_str)
+        # check if more than 2 fastq files exist per sample and if so concatenate all R1 and R2 fastq files with comma 
+        if len(fastq_files) > 2:
+            leftFastq = ",".join([x for x in fastq_files if "R1" in x])  # TODO check if lanes are always ordered correctly??
+            rightFastq = ",".join([x for x in fastq_files if "R2" in x])
+            fastq_files = [leftFastq,rightFastq]
+        config["samples"][sample] = fastq_files
